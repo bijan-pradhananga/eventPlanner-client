9613646439bef0895c0ab35a348c4fdede3bf25b
bijan_pradhananga <bijan2059@gmail.com>
2026-02-25 11:04:20 +0545

    add CreateEventPage and EditEventPage components for event creation and editing

---
+++ src/pages/events/CreateEventPage.tsx
@@ -0,0 +1,5 @@
+import EventFormPage from './EventFormPage';
+
+export default function CreateEventPage() {
+  return <EventFormPage mode="create" />;
+}
---
+++ src/pages/events/EditEventPage.tsx
@@ -0,0 +1,5 @@
+import EventFormPage from './EventFormPage';
+
+export default function EditEventPage() {
+  return <EventFormPage mode="edit" />;
+}
